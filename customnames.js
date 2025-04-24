f578d6654ef44d061a3464937544f8427496e1e4
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2025-04-24 14:54:09 -0500

    initial commit for custom name gradients system!

---
+++ customnames.js
@@ -0,0 +1,3 @@
+let allnames = ["MangoKitten","gamer223","apple"]
+let allnewnames = ["silyMangoKitten","hous e key","ana"]
+let allgradients = ["to right, #ff7300, #bb00ff", "to right, #084f00, #002d4f","to right, #ffffff, #ff59f1"]
