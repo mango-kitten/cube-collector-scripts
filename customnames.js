4f1215748d8d2c3d40af80678e97fdd4ff3a8753
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2025-04-24 15:00:50 -0500

    new formatting, should hopefully work

--- customnames.js
+++ customnames.js
@@ -1,3 +1,1 @@
-let allnames = ["MangoKitten","gamer223","apple"]
-let allnewnames = ["silyMangoKitten","hous e key","ana"]
-let allgradients = ["to right, #ff7300, #bb00ff", "to right, #084f00, #002d4f","to right, #ffffff, #ff59f1"]
+[["MangoKitten","gamer223","apple"], ["silyMangoKitten","hous e key","ana"], ["to right, #ff7300, #bb00ff", "to right, #084f00, #002d4f","to right, #ffffff, #ff59f1"]]
